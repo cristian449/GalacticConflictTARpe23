f31b02ebe5d3d4fd9c3ea60da6a8c55673500dc8
cristian449 <144561600+cristian449@users.noreply.github.com>
2024-11-27 15:42:19 +0200

    Added PlanetDto and PlanetID to FileToDatabase

---
+++ IntergalacticConflict.Core/Dto/PlanetDto.cs
@@ -0,0 +1,44 @@
+﻿using IntergalacticConflict.Core.Domain;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntergalacticConflict.Core.Dto
+{
+    public class PlanetDto
+    {
+        public Guid Id { get; set; }
+        public string PlanetName { get; set; }
+
+        public PlanetType PlanetType { get; set; }
+
+        public PlanetStatus PlanetStatus { get; set; }
+
+        public int PlanetPopulation { get; set; }
+
+        public Guid GalaxyID { get; set; }
+        public int Major_cities { get; set; }
+
+        public string CapitalCity { get; set; }
+
+        public int AmountOfShipsonPlanet { get; set; }
+
+        public string SpaceStation { get; set; }
+        // Might try adding  "public int SpaceStations { get; set; } To add multiple space stations to one planet
+
+        public SpaceStationType SpaceStationType { get; set; }
+
+
+        //IGMAE YAY
+        public List<IFormFile> Files { get; set; }
+
+        public IEnumerable<FileToDatabaseDto> Image { get; set; } = new List<FileToDatabaseDto>();
+
+        //Database only!
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+    }
+}
