cc22a0c49f4951b1f19a11257b90d2f8c6d47e3e
cristian449 <144561600+cristian449@users.noreply.github.com>
2024-12-05 11:09:05 +0200

    Fixed few new things

--- GalacticConflict/Controllers/AccountsController.cs
+++ GalacticConflict/Controllers/AccountsController.cs
@@ -12,8 +12,10 @@ namespace InterGalacticConflict.Controllers
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly InterGalacticConflictContext _context;
+
         public AccountsController
-            (UserManager<ApplicationUser> userManager,
+            (
+            UserManager<ApplicationUser> userManager,
             SignInManager<ApplicationUser> signInManager,
             InterGalacticConflictContext context
             )
@@ -33,6 +35,7 @@ namespace InterGalacticConflict.Controllers
             }
             return View();
         }
+
         [HttpPost]
         public async Task<IActionResult> AddPassword(AddPasswordViewModel model)
         {
@@ -55,31 +58,31 @@ namespace InterGalacticConflict.Controllers
         }
 
         [HttpGet]
-        public IActionResult ChangePassword(ChangePasswordViewModel model) 
+        public IActionResult ChangePassword()
         {
             return View();
         }
 
         [HttpPost]
-        public async Task<IActionResult> ChangePassword(AddPasswordViewModel model)
+        public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
         {
-            if (ModelState.IsValid) 
+            if (ModelState.IsValid)
             {
-                var user = await _userManager.GetUserAsync (User);
-                if (user != null) 
+                var user = await _userManager.GetUserAsync(User);
+                if (user == null)
                 {
                     return RedirectToAction("Login");
                 }
                 var result = await _userManager.ChangePasswordAsync(user, model.CurrentPassword, model.NewPassword);
                 if (!result.Succeeded)
                 {
-                    foreach(var error in result.Errors)
+                    foreach (var error in result.Errors)
                     {
-                        ModelState.AddModelError (string.Empty, error.Description);
+                        ModelState.AddModelError(string.Empty, error.Description);
                     }
                     return View();
                 }
-                await _signInManager.RefreshSignInAsync (user);
+                await _signInManager.RefreshSignInAsync(user);
                 return View("ChangePasswordConfirmation");
             }
             return View(model);
@@ -92,142 +95,26 @@ namespace InterGalacticConflict.Controllers
             return View();
         }
 
-        [HttpGet]
+        [HttpPost]
         [AllowAnonymous]
         public async Task<IActionResult> ForgotPassword(ForgotPasswordViewModel model)
         {
-            if (ModelState.IsValid) 
+            if (ModelState.IsValid)
             {
                 var user = await _userManager.FindByEmailAsync(model.Email);
 
                 if (user != null && await _userManager.IsEmailConfirmedAsync(user))
                 {
-                    var token = await _userManager.GenerateChangeEmailTokenAsync(user);
+                    var token = await _userManager.GeneratePasswordResetTokenAsync(user);
 
-                    var passwordResetLink = Url.Action("ResetPassword", "Accounts", new {email = model.Email, token = token}, Request.Scheme);
+                    var passwordResetLink = Url.Action("ResetPassword", "Accounts", new { email = model.Email, token = token }, Request.Scheme);
+                    // !!
 
                     return View("ForgotPasswordConfirmation");
                 }
                 return View("ForgotPasswordConfirmation");
-
             }
             return View(model);
-        }
-
-        [HttpGet]
-        [AllowAnonymous]
-        public IActionResult ResetPasswordConfirmation() { return View(); }
-
-        // user register methods
-        [HttpGet]
-        public IActionResult Register()
-        {
-            return View();
-        }
-        [HttpPost]
-        [AllowAnonymous]
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public async Task<IActionResult> Register(RegisterViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                var user = new ApplicationUser()
-                {
-                    UserName = model.Email,
-                    Email = model.Email,
-                    City = model.City,
-                };
-                var result = await _userManager.CreateAsync(user, model.Password);
-                if (result.Succeeded)
-                {
-                    var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    var confirmationLink = Url.Action("ConfirmEmail", "Accounts", new { userId = user.Id, token = token }, Request.Scheme);
-                    if (_signInManager.IsSignedIn(User) && User.IsInRole("Admin"))
-                    {
-                        return RedirectToAction("ListUsers", "Administrations");
-                    }
-                    ViewBag.ErrorTitle = "You have successfully registered";
-                    ViewBag.ErrorMessage = "Before you can log in, please confirm email from the link" +
-                        "\nwe have emailed to your email address.";
-                    return View("Error");
-                }
-                foreach (var error in result.Errors)
-                {
-                    ModelState.AddModelError("", error.Description);
-                }
-            }
-            return View();
-        }
-        [HttpGet]
-        [AllowAnonymous]
-        public async Task<IActionResult> ConfirmEmail(string userId, string token)
-        {
-            if (userId == null || token == null) { return RedirectToAction("Index", "Home"); }
-            var user = await _userManager.FindByIdAsync(userId);
-            if (user == null)
-            {
-                ViewBag.ErrorMessage = $"The user with id of {userId} is not valid";
-                return View("NotFound");
-            }
-            var result = await _userManager.ConfirmEmailAsync(user, token);
-            if (result.Succeeded)
-            {
-                return View();
-            }
-            ViewBag.ErrorTitle = "Email cannot be confirmed";
-            ViewBag.ErrorMessage = $"The users email, with userid of {userId}, cannot be confirmed.";
-            return View("Error");
-        }
-        // user login & logout methods
-        [HttpGet]
-        [AllowAnonymous]
-        public async Task<IActionResult> Login(string? returnURL)
-        {
-            LoginViewModel vm = new()
-            {
-                ReturnURL = returnURL,
-                // extval
-            };
-            return View(vm);
-        }
-        [HttpPost]
-        [AllowAnonymous]
-        public async Task<IActionResult> Login(LoginViewModel model, string? returnURL)
-        {
-            // extval
-            if (ModelState.IsValid)
-            {
-                var user = await _userManager.FindByEmailAsync(model.Email);
-                if (user != null && !user.EmailConfirmed && (await _userManager.CheckPasswordAsync(user, model.Password)))
-                {
-                    ModelState.AddModelError(string.Empty, "Your email hasn't been confirmed yet. Please check your Email spam folders.");
-                    return View(model);
-                }
-                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, true);
-                if (result.Succeeded)
-                {
-                    if (!string.IsNullOrEmpty(returnURL) && Url.IsLocalUrl(returnURL))
-                    {
-                        return Redirect(returnURL);
-                    }
-                    else
-                    {
-                        return RedirectToAction("Index", "Home");
-                    }
-                }
-                if (result.IsLockedOut)
-                {
-                    return View("AccountLocked");
-                }
-                ModelState.AddModelError("", "Invalid Login Attempt, please contact admin.");
-            }
-            return View(model);
-        }
-        [HttpPost]
-        public async Task<IActionResult> Logout()
-        {
-            await _signInManager.SignOutAsync();
-            return RedirectToAction("Index", "Home");
         }
 
         [HttpGet]
@@ -247,6 +134,7 @@ namespace InterGalacticConflict.Controllers
             };
             return View(model);
         }
+
         [HttpPost]
         [AllowAnonymous]
         public async Task<IActionResult> ResetPassword(ResetPasswordViewModel model)
@@ -276,9 +164,136 @@ namespace InterGalacticConflict.Controllers
                 await _userManager.DeleteAsync(user);
                 return RedirectToAction("ResetPasswordConfirmation", "Accounts");
             }
+
             return RedirectToAction("ResetPasswordConfirmation", "Accounts");
         }
 
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult ResetPasswordConfirmation()
+        {
+            return View();
+        }
+
+        // user register methods
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    City = model.City,
+                };
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+
+                    var confirmationLink = Url.Action("ConfirmEmail", "Accounts", new { userId = user.Id, token = token }, Request.Scheme);
+                    if (_signInManager.IsSignedIn(User) && User.IsInRole("Admin"))
+                    {
+                        return RedirectToAction("ListUsers", "Administrations");
+                    }
+
+                    ViewBag.ErrorTitle = "You have successfully registered";
+                    ViewBag.ErrorMessage = "Before you can log in, please confirm email from the link" +
+                        "\nwe have emailed to your email address.";
+                    return View("Error");
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View();
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> ConfirmEmail(string userId, string token)
+        {
+            if (userId == null || token == null) { return RedirectToAction("Index", "Home"); }
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                ViewBag.ErrorMessage = $"The user with id of {userId} is not valid";
+                return View("NotFound");
+            }
+            var result = await _userManager.ConfirmEmailAsync(user, token);
+            if (result.Succeeded)
+            {
+                return View();
+            }
+            ViewBag.ErrorTitle = "Email cannot be confirmed";
+            ViewBag.ErrorMessage = $"The users email, with userid of {userId}, cannot be confirmed.";
+            return View("Error");
+        }
+
+        // user login & logout methods
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(string? returnURL)
+        {
+            LoginViewModel vm = new()
+            {
+                ReturnURL = returnURL,
+                // extval
+            };
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(LoginViewModel model, string? returnURL)
+        {
+            // extval
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(model.Email);
+
+                if (user != null && !user.EmailConfirmed && (await _userManager.CheckPasswordAsync(user, model.Password)))
+                {
+                    ModelState.AddModelError(string.Empty, "Your email hasn't been confirmed yet. Please check your Email spam folders.");
+                    return View(model);
+                }
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, true);
+                if (result.Succeeded)
+                {
+                    if (!string.IsNullOrEmpty(returnURL) && Url.IsLocalUrl(returnURL))
+                    {
+                        return Redirect(returnURL);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                if (result.IsLockedOut)
+                {
+                    return View("AccountLocked");
+                }
+                ModelState.AddModelError("", "Invalid Login Attempt, please contact admin.");
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
 
     }
 }
--- GalacticConflict/Models/Accounts/AddPasswordViewModel.cs
+++ GalacticConflict/Models/Accounts/AddPasswordViewModel.cs
@@ -8,6 +8,8 @@ namespace InterGalacticConflict.Models.Accounts
         [DataType(DataType.Password)]
         [Display(Name = "Type new password here: ")]
         public string NewPassword { get; set; }
+       
+        
         [DataType(DataType.Password)]
         [Display(Name = "Type it again to confirm: ")]
         [Compare("NewPassword", ErrorMessage = "The new password and its confirmation do not match. Please retry.")]
