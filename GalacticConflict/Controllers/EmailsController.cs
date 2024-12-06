1fab5ddad2437918092f4b5805e70139a28d7e73
cristian449 <144561600+cristian449@users.noreply.github.com>
2024-12-06 10:01:45 +0200

    Added Stuff for Email viewmodel view and such, later on send stuff to other branch, if possible

---
+++ GalacticConflict/Controllers/EmailsController.cs
@@ -0,0 +1,31 @@
+﻿using IntergalacticConflict.Core.Dto;
+using InterGalacticConflict.Models.Email;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InterGalacticConflict.Controllers
+{
+    public class EmailsController : Controller
+    {
+        private readonly IEmailsServices _emailsServices;
+        public EmailsController(IEmailsServices emailsServices)
+        {
+            _emailsServices = emailsServices;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SendEmail(EmailViewModel viewModel)
+        {
+            var dto = new EmailDto()
+            {
+                To = viewModel.To,
+                Subject = viewModel.Subject,
+                Body = viewModel.Body,
+            };
+            _emailsServices.SendEmail(dto);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ InterGalacticConflict.ApplicationServices/Services/EmailServices.cs
@@ -0,0 +1,47 @@
+﻿using IntergalacticConflict.Core.Dto;
+using MailKit.Net.Smtp;
+using Microsoft.Extensions.Configuration;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterGalacticConflict.ApplicationServices.Services
+{
+    public class EmailServices : IEmailServices
+    {
+        private readonly IConfiguration _configuration;
+
+
+        public EmailServices(IConfiguration configuration)
+        {
+             _configuration = configuration;
+        }
+
+        public void SentEmail(EmailDto dto)
+        {
+            var email = new MimeMessage();
+            
+            email.From.Add(MailboxAddress.Parse(_configuration.GetSection("EmailUserName").Value));
+            email.To.Add(MailboxAddress.Parse(dto.To));
+            email.Subject = dto.Subject;
+            email.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = dto.Body
+            };
+
+            using var smtp = new SmtpClient();
+
+            smtp.Connect(_configuration.GetSection("EmailHost").Value, 587, MailKit.Security.SecureSocketOptions.StartTls);
+            smtp.Authenticate(_configuration.GetSection("EmailUserName").Value, _configuration.GetSection("EmailPassword").Value);
+            smtp.Send(email);
+            smtp.Disconnect(true);
+
+        }
+    }
+
+
+    
+}
